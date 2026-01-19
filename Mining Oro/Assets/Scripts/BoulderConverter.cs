96e216ddbf70236f2579593fe1a6d6b8fd1d3519
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-18 18:52:23 -0800

    implemented boulder converter to double gold

--- Mining Oro/Assets/Scripts/BoulderConverter.cs
+++ Mining Oro/Assets/Scripts/BoulderConverter.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class BoulderConverter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject goldCoin;
+
+    private void HandleRock(GameObject rock)
     {
-        
+        Vector3 spawnPos = rock.transform.position;
+
+        Instantiate(goldCoin, spawnPos, Quaternion.identity);
+        Instantiate(goldCoin, spawnPos, Quaternion.identity);
+
+        if (goldCoin.TryGetComponent<Rigidbody>(out var rb))
+        {
+            rb.AddForce(Vector3.up * 3f, ForceMode.Impulse);
+        }
+
+        Destroy(rock);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Boulder"))
+        {
+            HandleRock(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Rock"))
+        {
+            HandleRock(other.gameObject);
+        }
     }
 }
